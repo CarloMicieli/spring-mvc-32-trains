6c9a6c2ef17f79759f650ec20d19556552c2224f
Carlo Micieli <carlo.micieli@gmail.com>
2012-07-16 17:40:34 +0200

    Initial commit for rolling stocks search service.

---
+++ trenako-services/src/main/java/com/trenako/services/RollingStockSearchServiceImpl.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2012 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.trenako.services;
+
+import org.springframework.stereotype.Service;
+
+import com.trenako.SearchCriteria;
+import com.trenako.entities.RollingStock;
+import com.trenako.results.PaginatedResults;
+import com.trenako.results.RangeRequest;
+
+/**
+ * 
+ * @author Carlo Micieli
+ *
+ */
+@Service("rollingStockSearchService")
+public class RollingStockSearchServiceImpl implements RollingStockSearchService {
+
+	@Override
+	public PaginatedResults<RollingStock> findByCriteria(SearchCriteria sc,
+			RangeRequest range) {
+		// TODO Auto-generated method stub
+		
+		
+		return null;
+	}
+
+}
