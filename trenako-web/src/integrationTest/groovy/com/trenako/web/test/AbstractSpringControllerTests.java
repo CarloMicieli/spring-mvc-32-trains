1ea5b108ecf94a707d7c5dd8b422a5e3b0f8a254
CarloMicieli <carlo.micieli@gmail.com>
2013-05-21 22:04:48 +0200

    Updated.

--- trenako-web/src/integrationTest/groovy/com/trenako/web/test/AbstractSpringControllerTests.java
+++ trenako-web/src/integrationTest/groovy/com/trenako/web/test/AbstractSpringControllerTests.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2012 the original author or authors.
+ * Copyright 2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,24 +26,21 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.trenako.web.config.WebConfig;
 
 /**
- * It includes the unit tests for {@code Spring MVC} annotations.
+ * Abstract class for unit tests for {@code Spring MVC} annotations.
  *
  * @author Carlo Micieli
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(
-		loader = MockWebApplicationContextLoader.class,
-		classes = {WebConfig.class, TestConfig.class})
+@ContextConfiguration(classes = {WebConfig.class, TestConfig.class})
 @ActiveProfiles("test")
-@MockWebApplication(name = "appServlet")
+@WebAppConfiguration
 public abstract class AbstractSpringControllerTests {
 
-	// the TestContext framework doesn't support WebApplicationContext yet:
-	// <https://jira.springsource.org/browse/SPR-5243>
 	@Autowired
 	private WebApplicationContext wac;
 	
